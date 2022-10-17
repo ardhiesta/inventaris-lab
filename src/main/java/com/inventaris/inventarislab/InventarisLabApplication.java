c865a8f3280097c256a4a49c86139ff2fd3e9ba5
ardhiesta <ardhi.mail@gmail.com>
2022-10-17 13:37:33 +0700

    first commit

---
+++ src/main/java/com/inventaris/inventarislab/InventarisLabApplication.java
@@ -0,0 +1,13 @@
+package com.inventaris.inventarislab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarisLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarisLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventaris/inventarislab/InventarisLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventaris.inventarislab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarisLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
